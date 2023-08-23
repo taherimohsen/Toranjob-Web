ad10d66d1c67001935d6712d1c999674fbf8852d
AmirMohammadHemmati <20amhs00@gmail.com>
2023-08-23 23:46:45 +0330

    add telegram colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "telegram-blue-primary": "#0088CC",
+        "telegram-blue-secondary": "#179CDE",
+        "telegram-gray": "#8C8C8C",
+        "telegram-dark": "#333333",
+        "telegram-light": "#FFFFFF",
+      },
+    },
   },
   plugins: [],
-}
-
+};
