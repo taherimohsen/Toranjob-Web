dd7520e218c45df795957efb6dc40c8b0050376e
AmirMohammadHemmati <20amhs00@gmail.com>
2023-08-24 18:11:44 +0330

    feat : navbar for wide and mobile screens

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-import 'boxicons'
+//Note : to run all your custom codes in project put them in the app function ;
+
+import 'boxicons';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 // import Swiper bundle with all modules installed
 import Swiper from 'swiper/bundle';
 // import styles bundle
 import 'swiper/css/bundle';
-
 
 // AOS is library for fade animation
 AOS.init();
@@ -21,7 +22,6 @@ AOS.init({
   disableMutationObserver: false, // disables automatic mutations' detections (advanced)
   debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
   throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-  
 
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 120, // offset (in px) from the original trigger point
@@ -31,27 +31,33 @@ AOS.init({
   once: false, // whether animation should happen only once - while scrolling down
   mirror: false, // whether elements should animate out while scrolling past them
   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
 });
 
-const swiperHeader = new Swiper(".header-swiper", {
-  direction: "vertical",
+const swiperHeader = new Swiper('.header-swiper', {
+  direction: 'vertical',
   spaceBetween: 30,
-  effect: "fade",
+  effect: 'fade',
   centeredSlides: true,
   autoplay: {
     delay: 3000,
     disableOnInteraction: false,
   },
   pagination: {
-    el: ".header-swiper-pagination",
+    el: '.header-swiper-pagination',
     clickable: true,
     renderBullet: function (index, className) {
-      return '<span class="' + className + '">'+ (index<10?0:"") + (index + 1) + "</span>";
+      return (
+        '<span class="' +
+        className +
+        '">' +
+        (index < 10 ? 0 : '') +
+        (index + 1) +
+        '</span>'
+      );
     },
   },
 });
-const swiperSection = new Swiper(".section-swiper", {
+const swiperSection = new Swiper('.section-swiper', {
   spaceBetween: 10,
   centeredSlides: true,
   autoplay: {
@@ -59,11 +65,18 @@ const swiperSection = new Swiper(".section-swiper", {
     disableOnInteraction: false,
   },
   pagination: {
-    el: ".section-swiper-pagination",
+    el: '.section-swiper-pagination',
     // type: 'progressbar',
     clickable: true,
     renderBullet: function (index, className) {
-      return '<span class="' + className + '">'+ (index<10?0:"") + (index + 1) + "</span>";
+      return (
+        '<span class="' +
+        className +
+        '">' +
+        (index < 10 ? 0 : '') +
+        (index + 1) +
+        '</span>'
+      );
     },
   },
   // navigation: {
@@ -71,3 +84,28 @@ const swiperSection = new Swiper(".section-swiper", {
   //   prevEl: ".swiper-button-prev",
   // },
 });
+
+
+// //toggle fixed menu
+function toggleMenu() {
+  const openMobileMenuBtn = document.getElementById('open-menu-button');
+  const closeMobileMenuBtn = document.getElementById('close-menu-button');
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  openMobileMenuBtn.addEventListener('click', () => {
+    mobileMenu.classList.remove('hidden');
+    mobileMenu.classList.add('block');
+
+  });
+  closeMobileMenuBtn.addEventListener('click', () => {
+    mobileMenu.classList.remove('block');
+    mobileMenu.classList.add('hidden');
+  });
+}
+
+// this is main project function
+function app() {
+  toggleMenu();
+}
+
+document.addEventListener('DOMContentLoaded', app);
