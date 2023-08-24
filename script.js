e3f8bf112864bb525c1b440c0f29bb951b271369
AmirMohammadHemmati <20amhs00@gmail.com>
2023-08-24 20:10:09 +0330

    feat : title in header with typewriter / refactor : in js file

--- script.js
+++ script.js
@@ -1,101 +1,18 @@
 //Note : to run all your custom codes in project put them in the app function ;
-
 import 'boxicons';
 import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-// import Swiper bundle with all modules installed
+import 'aos/dist/aos.css';
 import Swiper from 'swiper/bundle';
-// import styles bundle
 import 'swiper/css/bundle';
-
-// AOS is library for fade animation
-AOS.init();
-// You can also pass an optional settings object
-// below listed default settings
-AOS.init({
-  // Global settings:
-  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: 'aos-init', // class applied after initialization
-  animatedClassName: 'aos-animate', // class applied on animation
-  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 400, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-});
-
-const swiperHeader = new Swiper('.header-swiper', {
-  direction: 'vertical',
-  spaceBetween: 30,
-  effect: 'fade',
-  centeredSlides: true,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: '.header-swiper-pagination',
-    clickable: true,
-    renderBullet: function (index, className) {
-      return (
-        '<span class="' +
-        className +
-        '">' +
-        (index < 10 ? 0 : '') +
-        (index + 1) +
-        '</span>'
-      );
-    },
-  },
-});
-const swiperSection = new Swiper('.section-swiper', {
-  spaceBetween: 10,
-  centeredSlides: true,
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: '.section-swiper-pagination',
-    // type: 'progressbar',
-    clickable: true,
-    renderBullet: function (index, className) {
-      return (
-        '<span class="' +
-        className +
-        '">' +
-        (index < 10 ? 0 : '') +
-        (index + 1) +
-        '</span>'
-      );
-    },
-  },
-  // navigation: {
-  //   nextEl: ".swiper-button-next",
-  //   prevEl: ".swiper-button-prev",
-  // },
-});
-
-
-// //toggle fixed menu
+import Typewriter from 'typewriter-effect/dist/core';
+//handle toggle mobile menu
 function toggleMenu() {
   const openMobileMenuBtn = document.getElementById('open-menu-button');
   const closeMobileMenuBtn = document.getElementById('close-menu-button');
   const mobileMenu = document.getElementById('mobile-menu');
-
   openMobileMenuBtn.addEventListener('click', () => {
     mobileMenu.classList.remove('hidden');
     mobileMenu.classList.add('block');
-
   });
   closeMobileMenuBtn.addEventListener('click', () => {
     mobileMenu.classList.remove('block');
@@ -103,10 +20,100 @@ function toggleMenu() {
   });
 }
 
-// this is main project function
+// app is main function of project
 function app() {
+  // run commands of toggle menu function
   toggleMenu();
-  
-}
 
+  // title with typewriter
+  const headerTitle = document.getElementById('header-title');
+  const typewriter = new Typewriter(headerTitle, {
+    loop: true,
+    delay: 75,
+  });
+  typewriter
+    .pauseFor(2000)
+    .pauseFor(300)
+    .typeString('توران جاب')
+    .pauseFor(300)
+    .deleteChars(10)
+    .typeString('ربات تلگرامی کاریابی')
+    .typeString(' ایرانی')
+    .pauseFor(1000)
+    .start();
+
+  // AOS is library for fade animations
+  AOS.init();
+  // You can also pass an optional settings object
+  // below listed default settings
+  AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  });
+  // header slider
+  const swiperHeader = new Swiper('.header-swiper', {
+    direction: 'vertical',
+    spaceBetween: 30,
+    effect: 'fade',
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.header-swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return (
+          '<span class="' +
+          className +
+          '">' +
+          (index < 10 ? 0 : '') +
+          (index + 1) +
+          '</span>'
+        );
+      },
+    },
+  });
+  // sub slider
+  const swiperSection = new Swiper('.section-swiper', {
+    spaceBetween: 10,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.section-swiper-pagination',
+      // type: 'progressbar',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return (
+          '<span class="' +
+          className +
+          '">' +
+          (index < 10 ? 0 : '') +
+          (index + 1) +
+          '</span>'
+        );
+      },
+    },
+  });
+}
 document.addEventListener('DOMContentLoaded', app);
